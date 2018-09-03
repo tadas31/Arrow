f2c71747c9c9025626aa57f28a105944433723fe
Tadas Klavaitis <tadas.k31@gmail.com>
2018-09-03 21:51:01 +0300

    CON1 - in Progress Theme - Controls

---
+++ Assets/Scrips/Controls/Controls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+
+    Vector3 start = Vector3.zero;
+    Vector3 end = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            start = Input.mousePosition;
+
+            start = Camera.main.ScreenToWorldPoint(start);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            end = Input.mousePosition;
+
+            end = Camera.main.ScreenToWorldPoint(end);
+
+            Debug.DrawLine(start, end, Color.red);
+
+        }
+    }
+}
