1768a0085e8276c363f3a29d20bfdcc890410a11
Tadas Klavaitis <tadas.k31@gmail.com>
2018-09-04 18:40:15 +0300

    CON1 - Completed Theme - Controls

--- Assets/Scrips/Controls/Controls.cs
+++ Assets/Scrips/Controls/Controls.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,21 +16,50 @@ public class Controls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        //gets first point
         if(Input.GetMouseButtonDown(0))
         {
             start = Input.mousePosition;
-
-            start = Camera.main.ScreenToWorldPoint(start);
+            start = Camera.main.ScreenToWorldPoint(start).normalized;
         }
 
+        //gets last point
         if (Input.GetMouseButtonUp(0))
         {
             end = Input.mousePosition;
-
-            end = Camera.main.ScreenToWorldPoint(end);
-
-            Debug.DrawLine(start, end, Color.red);
-
+            end = Camera.main.ScreenToWorldPoint(end).normalized;
         }
+
+        //draws lines
+        Debug.DrawLine(Vector3.zero, Vector3.up, Color.blue);
+        Debug.DrawLine(Vector3.zero, Vector3.down, Color.blue);
+        Debug.DrawLine(start, end, Color.red);
+
+        //gets angle of line
+        float angle = Vector2.SignedAngle(start - end, Vector3.down);
+
+
+        Debug.Log("start   " + start + "  end  " + end);
+        Debug.Log("Kampas   " + angle);
+
+
+        //determines which arrow was selected
+        if (angle > -22.5 && angle < 22.5)
+            Debug.Log("up");
+        else if (angle > 22.5 && angle < 67.5)
+            Debug.Log("Right-up");
+        else if (angle > 67.5 && angle < 112.5)
+            Debug.Log("Right");
+        else if (angle > 112.5 && angle < 157.5)
+            Debug.Log("Right-down");
+        else if ((angle > 157.5 && angle < 180) || (angle > -180 && angle < -157.5))
+            Debug.Log("Down");
+        else if (angle > -157.5 && angle < -112.5)
+            Debug.Log("Left-down");
+        else if (angle > -112.5 && angle < -67.5)
+            Debug.Log("Left");
+        else if (angle > -67.5 && angle < -22.5)
+            Debug.Log("Left-up");
     }
 }
