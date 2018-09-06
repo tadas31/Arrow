064e3d224a98e2163a7802b96128b0b9d48c7c2e
Gabrielius Ulejevas <ule2ga@yahoo.com>
2018-09-06 23:37:43 +0300

    Minor changes

--- Assets/Scrips/GameManager.cs
+++ Assets/Scrips/GameManager.cs
@@ -10,14 +10,14 @@ public class GameManager : MonoBehaviour {
 
     GameObject[] arrows;//The massive in which all of the arrows is stored
     public int amountOfArrows = 3;// Change it later from public
+    int currentArrow = 0;
     float arrowHeigth = (5f * 3f) / 8f;//The height at witch the arrows are set
 
     // Use this for initialization
     void Start () {
         controlsScript = controls.GetComponent<Controls>();
-        GameObject arr = Instantiate(mainArrow, new Vector2(1f,5f),Quaternion.identity);
-        arrowScript = arr.GetComponent<ArrowScript>();
         ArrowInstanisation();
+        arrowScript = arrows[currentArrow].GetComponent<ArrowScript>();
     }
 	
 	// Update is called once per frame
@@ -32,6 +32,15 @@ public class GameManager : MonoBehaviour {
         if (arrowScript.position == controlsScript.dragPosition)
         {
             Debug.Log("Same position");
+            if (currentArrow == amountOfArrows - 1)
+            {
+                amountOfArrows++;
+                currentArrow = 0;
+                ArrowInstanisation();
+                arrowScript = arrows[currentArrow].GetComponent<ArrowScript>();
+            }
+            else
+                arrowScript = arrows[++currentArrow].GetComponent<ArrowScript>();
         }
         else {
             Debug.Log("Difrent");
@@ -44,13 +53,23 @@ public class GameManager : MonoBehaviour {
     /// </summary>
     public void ArrowInstanisation()
     {
+        if (arrows != null)
+        {
+            for (int i = 0; i < arrows.Length; i++)
+            {
+                Destroy(arrows[i]);
+            }
+        }
         //Calculating all of the width of the camera (5f is the camera size 2f is to both sides like from - 2.08 to 2.08)
         float cameraWidth = Camera.main.aspect * 5f *2f;
+        float scale = cameraWidth / (2 * amountOfArrows);
         arrows = new GameObject[amountOfArrows];
         for (int i = 0; i < amountOfArrows; i++)
         {
             //Sets it to the right position
             arrows[i] = Instantiate(mainArrow, new Vector2((cameraWidth/amountOfArrows*i) + (cameraWidth/ amountOfArrows / 2f) - cameraWidth/2f, arrowHeigth), Quaternion.identity);
+            //Scales down the arrows that all of them would fit
+            arrows[i].transform.localScale = new Vector2(scale,scale);
         }
     }
 }
