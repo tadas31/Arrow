a9221bde3f71f20f8baa28af663fc2427a3e2f20
Tadas Klavaitis <tadas.k31@gmail.com>
2018-09-21 20:05:07 +0300

    some changes to trail

---
+++ Assets/Scrips/DrawStaritLine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawStaritLine : MonoBehaviour {
+
+    public Transform trail;
+    float i = -14;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        trail.position = new Vector2(i, 0);
+        i += 0.2f;
+	}
+}
